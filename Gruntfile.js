f8a777ade1a1816a9174a4e6e6e8b56eb4050587
Corey ODonnell <rcoreyodonnell@gmail.com>
2016-05-14 20:25:36 -0400

    Added grunt task to view server

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = function(grunt) {
+    grunt.initConfig({
+        // TODO : Later
+        // sass: {
+        //     dist: {
+        //         files: {
+        //             'css/normalize.css' : 'sass/normalize.scss',
+        //             'css/style.css' : 'sass/style.scss'
+        //         },
+        //         options: {
+        //             style: 'expanded',
+        //             noCache: true,
+        //             sourcemap: 'none'
+        //         }
+        //     }
+        // },
+        connect: {
+            server: {
+                options: {
+                    port: 8080,
+                    keepalive: true,
+                    livereload: true
+                }
+            }
+        },
+        // TODO : Later
+        // watch: {
+        //     options: {
+        //         livereload: true
+        //     },
+        //     sass: {
+        //         files: ['sass/**/*.scss'],
+        //         tasks: ['sass'],
+        //     },
+        //     html: {
+        //         files: ['**/*.html']
+        //     }
+        // }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+
+    grunt.registerTask('default', ['sass', 'watch']);
+    grunt.registerTask('server', ['connect']);
+};
---
+++ src/js/app.js
@@ -0,0 +1,3 @@
+'use strict';
+
+var ngModule = angular.module('whoPaysNext', []);
