b66ccf3d25ca1ec2898942a43fecb8d3cb29eb92
rcodonnell <rcoreyodonnell@gmail.com>
2016-05-31 00:36:57 -0400

    Getting router to work

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,20 @@
 import { Component } from '@angular/core';
+import { Routes, ROUTER_DIRECTIVES } from '@angular/router';
 
+import { TestComponent } from './test.component'
 @Component({
-  selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+    selector: 'my-app',
+    template: `
+        <h1>My First Angular 2 App</h1>
+        <nav>
+            <a [routerLink]="['/']">home</a>
+            <a [routerLink]="['/test']">test</a>
+        </nav>
+        <router-outlet></router-outlet>
+    `,
+    directives: [ROUTER_DIRECTIVES]
 })
+@Routes([
+    {path: '/test', component: TestComponent}
+])
 export class AppComponent { }
