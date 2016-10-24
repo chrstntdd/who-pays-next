49b1c18ac34bc3c5a7283eee3f32ca80b4f29a31
rcodonnell <rcoreyodonnell@gmail.com>
2016-10-24 01:29:39 -0400

    building more of tour of heroes app

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,8 +1,13 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
 
 import {AppComponent} from './app.component';
+import {HeroDetailComponent} from './hero-detail.component';
+import {HeroesComponent} from './heroes.component';
+import {HeroService} from './hero.service';
+import {DashboardComponent} from './dashboard.component';
 
 /**
  * Entry point to application.
@@ -10,12 +15,34 @@ import {AppComponent} from './app.component';
 @NgModule({
     imports: [
         BrowserModule,
-        FormsModule
+        FormsModule,
+        RouterModule.forRoot([
+            {
+                path: 'heroes',
+                component: HeroesComponent
+            },
+            {
+                path: 'detail/:id',
+                component: HeroDetailComponent
+            },
+            {
+                path: 'dashboard',
+                component: DashboardComponent
+            },
+            {
+                path: '',
+                redirectTo: '/dashboard',
+                pathMatch: 'full'
+            }
+        ])
     ],
     declarations: [
         AppComponent,
-        HeroDetailComponent
+        HeroDetailComponent,
+        HeroesComponent,
+        DashboardComponent
     ],
+    providers: [HeroService],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
