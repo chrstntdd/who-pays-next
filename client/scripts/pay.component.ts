a6bad526a1f7f6a4171c691d9a3b693aafd1615c
rcodonnell <rcoreyodonnell@gmail.com>
2016-10-23 20:05:52 -0400

    removed old app to learn new angular changes

--- client/scripts/pay.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'pay-comp',
-  template: '<h2>You pay next</h2>'
-})
-export class PayComponent { }
--- client/scripts/test.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'test-comp',
-  template: '<h2>My Test comp</h2>'
-})
-export class TestComponent { }
