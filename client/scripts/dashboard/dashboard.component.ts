831807cf840efdf99d0c4db6d41daa73e246332e
Christian Todd <ChristianTodd27@icloud.com>
2016-11-20 16:55:20 -0500

    Change file extensions to .scss and adjust styleUrls in components accordingly.

--- client/scripts/dashboard/dashboard.component.ts
+++ client/scripts/dashboard/dashboard.component.ts
@@ -4,7 +4,7 @@ import {Router} from '@angular/router';
 @Component({
     selector: 'my-dashboard',
     templateUrl: 'dashboard.component.html',
-    styleUrls: ['dashboard.component.css']
+    styleUrls: ['dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit{
 
